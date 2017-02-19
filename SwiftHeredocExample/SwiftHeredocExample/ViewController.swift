7a73905818cd7556addb11b0021682d862094d07
Yusuke Hosonuma <tobi462@gmail.com>
2017-02-19 15:38:13 +0900

    Create Example project

---
+++ SwiftHeredocExample/SwiftHeredocExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftHeredocExample
+//
+//  Created by Yusuke on 2017/02/19.
+//  Copyright © 2017 Penginmura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SwiftHeredocExample/main.swift
@@ -0,0 +1,4 @@
+
+import Darwin
+
+print("Hello, World")
